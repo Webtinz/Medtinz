f7545aea8c759983e0896e410a9d37b27f852aeb
Archange DEGUENON <degittiq229@gmail.com>
2025-01-20 17:05:40 +0100

    Correction of token expired response

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -12,6 +12,10 @@ const authMiddleware = (req, res, next) => {
         return res.status(401).json({ message: 'Access denied. No token provided.' });
     }
 
+    if (error.name === 'TokenExpiredError') {
+        return res.status(401).json({ message: 'Votre session a expiré. Veuillez vous reconnecter.' });
+    }
+      
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET); // Utilisez votre clé secrète JWT
         req.user = decoded; // Ajoutez les données du token à l'objet req pour un accès ultérieur
