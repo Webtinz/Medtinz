4469449eb126ed5f4e555a9242a7d386a48e61f2
Archange DEGUENON <degittiq229@gmail.com>
2025-01-22 13:16:09 +0100

    Staff role assignment correction

--- src/views/clientviews/staffmanagment/adduserstaff.js
+++ src/views/clientviews/staffmanagment/adduserstaff.js
@@ -14,6 +14,7 @@ const animatedComponents = makeAnimated();
 
 
 const AddStaffModal = ({ visible, onClose, handleStaffAdded, initialData, setVisible, modalValue, handleButtonClick  }) => {
+
     // if (!visible) return null; // Ne montre pas le modal si `visible` est false
     const [formData, setFormData] = useState({
         hospital_id: initialData?.hospital_id || "",
@@ -86,8 +87,6 @@ const AddStaffModal = ({ visible, onClose, handleStaffAdded, initialData, setVis
         fetchData();
     }, [Hospitals]); // Ajout de Hospitals comme dépendance
     
-    // console.log(Hospitals[0]?._id);
-    
     const validateForm = () => {
         const newErrors = {};
         if (!formData.firstname) newErrors.firstname = "Firstname is required";
@@ -102,8 +101,11 @@ const AddStaffModal = ({ visible, onClose, handleStaffAdded, initialData, setVis
             newErrors.confirmPassword = "Passwords do not match";
         }
         if (!formData.specialties) newErrors.specialties = "Specialties is required";
-        // if (!formData.role) newErrors.role = "Role is required";
+        
         if (!modalValue && !formData.role) {
+            newErrors.role = "Role is required";
+        } 
+        if (modalValue ==="" || modalValue == 'Allusers') {
             newErrors.role = "Role is required";
         }        
         if (!formData.departementId) newErrors.departementId = "Department is required";
@@ -179,58 +181,59 @@ const handleSpecialtiesChange = (selectedOptions) => {
                 username: formData.username,
                 email: formData.email,
                 password: formData.password,
-                role: formData.role ?? modalValue,
+                // role: formData.role ?? modalValue,
+                role: modalValue ?? formData.role,
                 specialties: formData.specialties,
                 contact: formData.contact,
                 departementId: formData.departementId,
                 civility: formData.civility,
                 type: formData.type
             };
-            // console.log(formPayload);
+            console.log(formPayload);
             
-            try {
-                const response = await api.post('api/adduser', formPayload);
+            // try {
+            //     const response = await api.post('api/adduser', formPayload);
 
-                // console.log(response);
-                if ((response.status == 200 || response.status == 201) /*  && response.data.success */) {
-                    // Succès
+            //     // console.log(response);
+            //     if ((response.status == 200 || response.status == 201) /*  && response.data.success */) {
+            //         // Succès
                     
-                    toast.success("Staff registered successfully!");
-                    setFormData({
-                        hospital_id: "",
-                        firstname: "",
-                        lastname: "",
-                        username: "",
-                        email: "",
-                        password: "",
-                        confirmPassword: "",
-                        role: "",
-                        specialties: [],
-                        contact: {
-                            phone: "",
-                            address: ""
-                        },
-                        departementId: "", 
-                        type: "", 
-                        civility: "", 
-                    });
-                    handleStaffAdded();  // Callback après ajout du staff
-                    // response?.data?.user?._id
-                    // handleContinue(response?.data?.user?.hospital_id, response?.data?.user?._id);
-                    setTimeout(() => {
-                      handleContinue(response?.data?.user?.hospital_id, response?.data?.user?._id);
-                    }, 2000); // 2000 ms = 2 secondes
+            //         toast.success("Staff registered successfully!");
+            //         setFormData({
+            //             hospital_id: "",
+            //             firstname: "",
+            //             lastname: "",
+            //             username: "",
+            //             email: "",
+            //             password: "",
+            //             confirmPassword: "",
+            //             role: "",
+            //             specialties: [],
+            //             contact: {
+            //                 phone: "",
+            //                 address: ""
+            //             },
+            //             departementId: "", 
+            //             type: "", 
+            //             civility: "", 
+            //         });
+            //         handleStaffAdded();  // Callback après ajout du staff
+            //         // response?.data?.user?._id
+            //         // handleContinue(response?.data?.user?.hospital_id, response?.data?.user?._id);
+            //         setTimeout(() => {
+            //           handleContinue(response?.data?.user?.hospital_id, response?.data?.user?._id);
+            //         }, 2000); // 2000 ms = 2 secondes
 
-                    // onClose(); // Fermer la modal
-                } else {
-                    // Échec géré dans la réponse
-                    toast.error(response.data.message || "Failed to register staff. Please try again.");
-                }
-            } catch (error) {
-                toast.error(`Registration failed: ${error?.response?.data?.message || 'An unexpected error occurred. Please try again.'}`);
-                console.error('Error:', error?.response|| 'No error message available.');
+            //         // onClose(); // Fermer la modal
+            //     } else {
+            //         // Échec géré dans la réponse
+            //         toast.error(response.data.message || "Failed to register staff. Please try again.");
+            //     }
+            // } catch (error) {
+            //     toast.error(`Registration failed: ${error?.response?.data?.message || 'An unexpected error occurred. Please try again.'}`);
+            //     console.error('Error:', error?.response|| 'No error message available.');
 
-            }
+            // }
         }
     };
     return (
